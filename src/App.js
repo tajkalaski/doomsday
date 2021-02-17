90d06ab36bd464d0b978143c4821894b7e600973
tajkalaski <laski.tajka@gmail.com>
2021-02-17 18:03:23 +0100

    first sketch of the solar system with asteroid and meteors

---
+++ src/App.js
@@ -0,0 +1,24 @@
+const App = () => {
+  return (
+    <div id="universe">
+      <div id="sky">
+        <div id="meteor-1" class="meteor"></div>
+      </div>
+      <div id="galaxy">
+        <div id="earth"></div>
+        <div id="asteroid-1" class="asteroid-orbit">
+          <div class="asteroid-positon">
+            <div class="asteroid"></div>
+          </div>
+        </div>
+        <div id="asteroid-2" class="asteroid-orbit">
+          <div class="asteroid-positon">
+            <div class="asteroid"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
